bbb292170a6c3cec501861c374271e1dad6aebb7
Sallagar <ivankononenko772@gmail.com>
2023-10-01 19:16:07 +0000

    make а sample

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return(
+        <footer class="page-footer">
+            <div class="container">
+            © {new Date().getFullYear()} Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+        </footer>
+    )
+}
+
+export {Footer}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return  (
+        <nav>
+            <div class="nav-wrapper">
+            <a href="!#" class="brand-logo center">Movies</a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+            </div>
+        </nav>
+    )
+}
+
+export {Header}
